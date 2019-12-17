312fe8f4443f6c9e88880fbe3a1240f5fbd78caf
Vuzeta <machniik95@gmail.com>
2019-12-17 14:50:04 +0100

    build layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,13 @@
-import Scroller from './Scroller';
-import Swiper from './Swiper';
 import '../scss/main.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const scroller = new Scroller('#root');
-  new Swiper();
-
-  document.addEventListener('wheel', scroller.listenScroll);
-  document.addEventListener('swipeUp', () => scroller.scroll(1));
-  document.addEventListener('swipeDown', () => scroller.scroll(-1));
-  document.addEventListener('keydown', e => {
-    if (e.keyCode === 40) {
-      scroller.scroll(1);
-    } else if (e.keyCode === 38) {
-      scroller.scroll(-1);
-    } else {
-      return;
-    }
-  });
+var swiper = new Swiper('.swiper-container', {
+	pagination: '.swiper-pagination',
+	direction: 'vertical',
+	slidesPerView: 1,
+	paginationClickable: true,
+	mousewheelControl: true,
+	keyboardControl: true,
+	touchControl: false,
+	parallax: true,
+	speed: 600,
 });

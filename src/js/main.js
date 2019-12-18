958709daa6c2732a79adf52749bd3a91f208ce1b
Vuzeta <machniik95@gmail.com>
2019-12-18 13:25:43 +0100

    add monochrome slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import '../scss/main.scss';
 
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.s1', {
 	pagination: '.swiper-pagination',
 	direction: 'vertical',
 	slidesPerView: 1,
@@ -10,4 +10,14 @@ var swiper = new Swiper('.swiper-container', {
 	touchControl: false,
 	parallax: true,
 	speed: 600,
+});
+
+var mySwiper2 = new Swiper('.s2', {
+	loop: true,
+	autoplay: 4000,
+	speed: 1000,
+	direction: 'horizontal',
+	pagination: '.swiper-pagination2',
+	scrollbar: '.swiper-scrollbar2',
+	paginationClickable: true,
 });
